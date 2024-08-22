c91f9fb829aa536c327ff5aaab21bee3746aaebd
Leziga Bakor <leziga.bakor@gmail.com>
2024-08-22 23:46:47 +0100

    feat: ordered post from most recent

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 # Create your views here.
 def home(request):
-    context = { 
+    context = {
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
@@ -14,5 +14,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model><viewtype>.html
     context_object_name = 'posts'
+    ordering=['-date_posted']
+
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
