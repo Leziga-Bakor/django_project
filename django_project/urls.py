4d8233306e5273e90c9dbc8aa2af990504de648e
Leziga Bakor <leziga.bakor@gmail.com>
2024-09-08 21:48:10 +0100

    feat: add password reset url

--- django_project/urls.py
+++ django_project/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout/', user_views.logout_view, name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+         name='password_reset'),
     path('', include('blog.urls')),
 ]
 
